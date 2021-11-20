f234b4b35db570bac0246b419deb8630f1e5e6ac
JohnSalinas123 <salinasjohn257@gmail.com>
2021-11-20 13:45:40 -0800

    added some test code for ListGraph

--- DriverAG.java
+++ DriverAG.java
@@ -2,7 +2,28 @@ public class DriverAG {
   
     public static void main(String []args) {
 
-        
+        ListGraph graphList = new ListGraph(4);
+
+        graphList.setLabel(0,0);
+        graphList.setLabel(1,1);
+        graphList.setLabel(2,2);
+        graphList.setLabel(3,3);
+
+        graphList.addEdge(0,0);
+        graphList.addEdge(0,2);
+        graphList.addEdge(1,0);
+        graphList.addEdge(2,3);
+        graphList.addEdge(3,0);
+        graphList.addEdge(3,2);
+
+        System.out.println(graphList.isEdge(0,2));
+
+        //graphList.removeEdge(0, 2);
+
+        System.out.println(graphList.isEdge(0, 2));
+
+        graphList.printGraph();
+
 
     }
 
